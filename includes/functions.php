5ce8218955daa737f3175ac8a59e69d212d2b917
Asko Nõmm <asko@digitalpress.co>
2017-01-22 22:41:49 +0200

    bump

--- includes/functions.php
+++ includes/functions.php
@@ -2,14 +2,15 @@
 
 function viaplay_import($file) {
 
+	// get items
 	$reader = new \EasyCSV\Reader($file);
 	$items = [];
 
 	while($row = $reader->getRow()) {
 
-		$items[] = (object) [
+		$items[] = [
 			'id' => $row['id'],
-			'pealkiri' => $row['title'],
+			'pealkiri' => $row['pealkiri'],
 			'pakett' => $row['pakett'],
 			'code' => $row['code'],
 			'state' => $row['state'],
@@ -21,7 +22,26 @@ function viaplay_import($file) {
 
 	}
 
-	var_dump($items);
-	die();
+	// configure ORM
+	ORM::configure('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME);
+	ORM::configure('username', DB_USER);
+	ORM::configure('password', DB_PASSWORD);
+	ORM::configure('driver_options', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+
+	foreach($items as $item) {
+
+		$voucher = ORM::for_table('vouchers')->create();
+		$voucher->voucher_id = $item['id'];
+		$voucher->voucher_pealkiri = $item['pealkiri'];
+		$voucher->voucher_pakett = $item['pakett'];
+		$voucher->voucher_code = $item['code'];
+		$voucher->voucher_state = $item['state'];
+		$voucher->voucher_userId = $item['userId'];
+		$voucher->voucher_validityStart = $item['validityStart'];
+		$voucher->voucher_validityEnd = $item['validityEnd'];
+		$voucher->voucher_price = $item['price'];
+		$voucher->save();
+
+	}
 
 }
--- viaplay-import.php
+++ viaplay-import.php
@@ -9,6 +9,7 @@ define('VI_DIR', __DIR__);
 require_once VI_DIR . '/includes/EasyCSV/AbstractBase.php';
 require_once VI_DIR . '/includes/EasyCSV/Reader.php';
 require_once VI_DIR . '/includes/EasyCSV/Writer.php';
+require_once VI_DIR . '/includes/idiorm/idiorm.php';
 require_once VI_DIR . '/includes/functions.php';
 require_once VI_DIR . '/includes/actions.php';
 require_once VI_DIR . '/includes/filters.php';
