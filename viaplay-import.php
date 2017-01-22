aa843bf57d8604327de8f98d5781fae9e9a8e04d
Asko Nõmm <asko@digitalpress.co>
2017-01-22 19:39:57 +0200

    bump

---
+++ viaplay-import.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Viaplay Import
+ */
+
+define('VI_DIR', __DIR__);
+
+require_once VI_DIR . '/includes/functions.php';
+require_once VI_DIR . '/includes/actions.php';
+require_once VI_DIR . '/includes/filters.php';
